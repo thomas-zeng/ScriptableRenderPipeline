8fbfd1fbb2206cabc1f7a458066128a403ebf118
sebastienlagarde <sebastien@unity3d.com>
2019-08-07 00:45:12 +0200

    Update SkyRenderer.cs

--- com.unity.render-pipelines.high-definition/Runtime/Sky/SkyRenderer.cs
+++ com.unity.render-pipelines.high-definition/Runtime/Sky/SkyRenderer.cs
@@ -13,16 +13,16 @@ namespace UnityEngine.Rendering.HighDefinition
         public abstract void Cleanup();
 
         /// <summary>
-        /// Called every frame exactly once. Implement it if your SkyRenderer needs to iterate independently from the user defined update frequency (see SkySettings UpdateMode)
+        /// HDRP calls this function once every frame. Implement it if your SkyRenderer needs to iterate independently of the user defined update frequency (see SkySettings UpdateMode).
         /// </summary>
         /// <returns>True if the update determines that sky lighting needs to be re-rendered. False otherwise.</returns>
         public virtual bool Update(BuiltinSkyParameters builtinParams) { return false; }
 
         /// <summary>
-        /// Implements actual rendering of the sky. This will be called when rendering the sky into a cubemap (for lighting) and also during the main frame rendering.
+        /// Implements actual rendering of the sky. HDRP calls this when rendering the sky into a cubemap (for lighting) and also during main frame rendering.
         /// </summary>
-        /// <param name="builtinParams">Engine parameters provided to users for rendering the sky.</param>
-        /// <param name="renderForCubemap">This will be true when the sky is being rendered into a cubemap for lighting. This is useful when the sky renderer needs a different implementation in this case.</param>
+        /// <param name="builtinParams">Engine parameters that you can use to render the sky.</param>
+        /// <param name="renderForCubemap">Pass in true if you want to render the sky into a cubemap for lighting. This is useful when the sky renderer needs a different implementation in this case.</param>
         /// <param name="renderSunDisk">If the sky renderer supports the rendering of a sun disk, it must not render it if this is set to false.</param>
         public abstract void RenderSky(BuiltinSkyParameters builtinParams, bool renderForCubemap, bool renderSunDisk);
 
